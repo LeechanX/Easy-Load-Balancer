b88f4cd5abeca3a692230ad3234f1540bc42d0d3
lichenxuan <lichenxuan@xiaomi.com>
2017-09-03 19:56:31 +0800

    c++ api do example

--- api/cpp/example.cc
+++ api/cpp/example.cc
@@ -1,16 +1,27 @@
+#include <stdlib.h>
 #include "elbApi.h"
 #include <iostream>
 
 int main(int argc, char const *argv[])
 {
+    int modid = atoi(argv[1]);
+    int cmdid = atoi(argv[2]);
     elbClient client;
     std::string ip;
     int port;
-    int ret = client.apiGetHost(10001, 1001, 10, ip, port);
+    int ret = client.apiGetHost(modid, cmdid, 10, ip, port);
     if (ret == 0)
     {
         std::cout << "host is " << ip << ":" << port << std::endl;
-        client.apiReportRes(10001, 1001, ip, port, 0);
+        client.apiReportRes(modid, cmdid, ip, port, 0);
+    }
+    else if (ret == -9998)
+    {
+        std::cout << modid << "," << cmdid << " not exist" << std::endl;
+    }
+    else
+    {
+        std::cout << "get error code " << ret << std::endl;
     }
     return 0;
 }
